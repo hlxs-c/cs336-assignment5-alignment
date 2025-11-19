357fcd61c6fb47fac799dd1faabd453c83e0ff03
cfc <cfc2021007@163.com>
2025-11-20 01:35:39 +0800

    fix: 修改了reward_fn_test.py 中使用的测试文件 test_reward_fn.jsonl 的路径；
    Note：这里使用的是绝对路径，实际上应该使用相对路径，但是为了单步调试，使用了绝对路径（实际上也可以使用像对路径，在代码前添加文件目录就行）；

--- cs336_alignment/debug/reward_fn_test.py
+++ cs336_alignment/debug/reward_fn_test.py
@@ -2,7 +2,7 @@ import json
 
 from cs336_alignment.drgrpo_grader import r1_zero_reward_fn
 
-with open("/home/op/cfc/llm/cs336-assignment5-alignment/data/test_reward_fn.jsonl", mode="r", encoding="utf-8") as f:
+with open("/home/op/cfc/llm/cs336-assignment5-alignment/output/test_reward_fn.jsonl", mode="r", encoding="utf-8") as f:
   for line in f:
     data = json.loads(line.strip())
     print(f'--------------------- answer -----------------')
